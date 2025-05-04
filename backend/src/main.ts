b6882ca2014af1dc7f9710e9226acd2e23011f78
Lucas Ioran Marciano <lucas.marciano99@outlook.com>
2025-05-04 00:08:56 -0300

    refactor(main): mock data to not consult gemini API every time

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import fs from "fs";
 import dotenv from "dotenv";
 import { PROMPT_ANALYSE_BILL } from "./config/index.js";
 import { GeminiService } from "./services/GeminiService.js";
+import { getJsonBlockWithNewlines } from "./utils/json-parser.js";
 
 dotenv.config();
 
@@ -20,13 +21,21 @@ if (!fs.existsSync(image_path)) {
 const geminiService = new GeminiService();
 
 async function main() {
-  const response = await geminiService.analyzeImage(
-    image_path,
-    PROMPT_ANALYSE_BILL
-  );
-  console.log(response);
+  //   const response = await geminiService.analyzeImage(
+  //     image_path,
+  //     PROMPT_ANALYSE_BILL
+  //   );
+  //   console.log(response);
+  const response = `\`\`\`json
+{
+    "valor_total": "115,06",
+    "mes_referencia": "Abril/2025"
+}
+\`\`\``;
+  const responseFormated = getJsonBlockWithNewlines(response);
 
-  return response;
+  return responseFormated;
 }
 
-await main();
+const a = await main();
+console.log(a);
