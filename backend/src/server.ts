b8d3b42930f1263d3ba29c6ead01bb6c322feea0
Lucas Ioran Marciano <lucas.marciano99@outlook.com>
2025-05-05 13:39:20 -0300

    refactor(app)!: change entry point to server.ts and update package scripts

---
+++ backend/src/server.ts
@@ -0,0 +1,75 @@
+// import path, { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import fs from "fs";
+// import dotenv from "dotenv";
+// import { PROMPT_ANALYSE_BILL, UPLOAD_DIR } from "./config/index.js";
+// import { GeminiService } from "./services/GeminiService.js";
+// import { getJsonBlockWithNewlines } from "./utils/json-parser.js";
+
+// dotenv.config();
+
+// const image_path = path.join(UPLOAD_DIR, "teste.png");
+// if (!fs.existsSync(image_path)) {
+//   console.error(`Erro: Arquivo de imagem não encontrado em: ${image_path}`);
+//   process.exit(1); // Sair se a imagem não existir
+// }
+
+// const geminiService = new GeminiService();
+
+// async function main() {
+//   //   const response = await geminiService.analyzeImage(
+//   //     image_path,
+//   //     PROMPT_ANALYSE_BILL
+//   //   );
+//   //   console.log(response);
+//   const response = `\`\`\`json
+// {
+//     "valor_total": "115,06",
+//     "mes_referencia": "Abril/2025"
+// }
+// \`\`\``;
+//   const responseFormated = getJsonBlockWithNewlines(response);
+
+//   return responseFormated;
+// }
+
+// const a = await main();
+// console.log(a);
+
+import "reflect-metadata";
+import express, {
+  Request,
+  Response,
+  NextFunction,
+  ErrorRequestHandler,
+} from "express";
+// import cors from 'cors';
+import "dotenv/config"; // Garante que dotenv carregue antes de tudo
+import mainRouter from "./routes/index.js"; // Importa o roteador principal - Verifique extensão .js
+import errorHandler from "./middlewares/errorHandler.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middlewares Essenciais
+// app.use(cors()); // Habilita CORS para todas as origens (ajuste em produção!)
+app.use(express.json({ limit: "10mb" })); // Habilita parsing de JSON no body (aumente o limite se base64 for grande)
+app.use(express.urlencoded({ extended: true })); // Para parsing de application/x-www-form-urlencoded
+
+// Rotas Principais (sem prefixo global aqui, o prefixo está dentro de routes/index.ts se necessário)
+app.use(mainRouter); // <<< USA O ROTEADOR PRINCIPAL AQUI
+
+// Rota "Not Found" (Opcional, mas bom ter)
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Endpoint não encontrado." });
+});
+
+// Middleware Global de Tratamento de Erros (IMPORTANTE: Deve ser o ÚLTIMO middleware)
+app.use(errorHandler);
+
+// Inicialização do Servidor
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+});
+
+export default app; // Exportar app pode ser útil para testes
